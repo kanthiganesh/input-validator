cc415f89c0ec3430eb6beb9215b2c181f00ab71e
Ganesh <ganesh.kanthi@plugxr.com>
2023-02-14 17:27:12 +0530

    added isValidUrl function

---
+++ index.js
@@ -0,0 +1,8 @@
+export function isValidUrl(url){
+    var expression =/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+    if(!url.match(regex)) 
+       return false;
+    else 
+        return true;
+}
